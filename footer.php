c84deeced01eaab41e6a67aba6b76d65f9a7aff9
Damian Moser <dmo140748@stud.gibb.ch>
2024-01-28 19:14:28 +0100

    link farben

--- footer.php
+++ footer.php
@@ -1,10 +1,10 @@
     <footer>
         <div id="footer">
             <div id="footer-wrapper">
-                <a href="<?php echo home_url(); ?>" id="home-link">
-                    <img src="<?php echo get_template_directory_uri() . "/assets/img/" ?>wachseldorn.svg" alt="Home">
+                <a href="<?= home_url(); ?>" id="home-link">
+                    <img src="<?= get_template_directory_uri() . "/assets/img/" ?>wachseldorn.svg" alt="Home">
                 </a>
-                <a id="admin" href="<?php echo home_url() . "/wp-admin/"; ?>" target="_blank" rel="noopener">Login</a>
+                <a class="link-white" href="<?= home_url() . "/wp-admin/"; ?>" target="_blank" rel="noopener">Login➚</a>
             </div>
             <?php
                 wp_nav_menu(array(
@@ -14,11 +14,11 @@
             ?>
             <hr>
             <div id="footer-cred">
-                <p>Gemeinde Wachseldorn</p>
-                <a href="https://damian.work" target="_blank" rel="noopener">&copy;2024 by Damian</a>
+                <a class="link-white" href="<?= home_url(); ?>">Gemeinde Wachseldorn</a>
+                <a class="link-white" href="https://damian.work" target="_blank" rel="noopener">&copy;2024 by Damian</a>
                 <div>
-                    <a href="<?php echo home_url() . '/datenschutz' ?>">Datenschutzerklärung</a><span style="color: #ffffff;"> |</span>
-                    <a href="<?php echo home_url() . '/impressum' ?>">Impressum</a>
+                    <a class="link-white" href="<?php echo home_url() . '/datenschutz' ?>">Datenschutzerklärung</a><span style="color: #ffffff;"> |</span>
+                    <a class="link-white" href="<?php echo home_url() . '/impressum' ?>">Impressum</a>
                 </div>
             </div>
         </div>
